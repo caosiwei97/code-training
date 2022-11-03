a9c8cd8524c1dcc6638514d48bb049432120477d
5C24 <You_5c24@163.com>
2022-11-03 23:18:03 +0800

    Update lazyMan.js

--- handwrite-weekly/week-03/code/lazyMan.js
+++ handwrite-weekly/week-03/code/lazyMan.js
@@ -1,22 +1,3 @@
-// LazyMan("Hank");
-// // Hi! This is Hank!
-
-// LazyMan("Hank").sleep(10).eat("dinner");
-// // Hi! This is Hank!
-// // 等待10秒..
-// // Wake up after 10
-// // Eat dinner~
-
-// LazyMan("Hank").eat("dinner").eat("supper");
-// // Hi This is Hank!
-// // Eat dinner~
-// // Eat supper~
-
-// LazyMan("Hank").sleepFirst(5).eat("supper");
-// 等待5秒
-// Wake up after 5
-// Hi This is Hank!
-// Eat supper
 class Lazy {
   constructor(name) {
     this.tasks = [];
